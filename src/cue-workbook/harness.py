82375c2e44eeacee6544bdda8d24ba34a3ed7d2b
Bruce Lavoie <f4t.bruce@gmail.com>
2026-07-17 12:26:34 -0400

    Expose S04 package execution through workbook harness

--- src/cue-workbook/harness.py
+++ src/cue-workbook/harness.py
@@ -3,6 +3,7 @@ from models import DEFAULT_WORKBOOK_REQUEST, HarnessError, _reject_claimant_fiel
 from native import DirectSession, NativeBindingUnavailable, summarize_value
 from native_backend import gopy_worker_main
 from native_validation import execute_native_probe
+from s04_package import execute_s04_package, s04_judge_worker_main
 from validation import execute_probe, run_architecture_validation
 
 __all__ = [
@@ -13,7 +14,9 @@ __all__ = [
     "_reject_claimant_fields",
     "execute_native_probe",
     "execute_probe",
+    "execute_s04_package",
     "gopy_worker_main",
     "run_architecture_validation",
+    "s04_judge_worker_main",
     "summarize_value",
 ]
